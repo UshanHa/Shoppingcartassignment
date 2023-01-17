8cd065940eef129d610ecc93a365f71c948f8ae4
UshanHa <56099026+UshanHa@users.noreply.github.com>
2023-01-16 20:18:41 -0800

    Add files via upload

---
+++ ItemOrder.java
@@ -0,0 +1,25 @@
+//Sadman Sakib Hassan
+
+public class ItemOrder {
+    private Item item;
+    private int quantity;
+    
+    /**
+    * 
+    * @param item
+    * @param quantity
+    */
+    public ItemOrder(Item item, int quantity) {
+    this.item = item;
+    this.quantity = quantity;
+    }
+    
+    public double getPrice() {
+    return item.priceFor(quantity);
+    }
+    
+    public Item getItem() {
+    return item;
+    }
+    
+    }
